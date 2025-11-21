8169034a8f71f022ede8503b050fa323d93aa170
Mya <mya.florescastanon@cesunbc.edu.mx>
2025-11-20 19:35:39 -0800

    Modificaciones

--- core_main_RH/settings.py
+++ core_main_RH/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = 'django-insecure-a%!6*3!w86tz-ro94#!ys-*q2^r*ugq2b4u0d2kj8r)tsrc)50
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['MyaFlores05.pythonanywhere.com']
 
 
 # Application definition
