4b4410cc0e79fc3f75e6542c137fca29da910238
Mya <mya.florescastanon@cesunbc.edu.mx>
2025-12-07 21:42:17 -0800

    Actualizacion de base de datos

--- admin_core_RH/admin.py
+++ admin_core_RH/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from .models import *
+from .models import Empleado, Registro, Departamento, Usuario, Asistencia, Horario, Permiso, Notificacion
 
-admin.site.register(Empleado )
- 
-admin.site.register(Registro )
-
-admin.site.register(Departamento )
-
+admin.site.register(Empleado)
+admin.site.register(Registro)
+admin.site.register(Departamento)
 admin.site.register(Usuario)
+admin.site.register(Asistencia)
+admin.site.register(Horario)
+admin.site.register(Permiso)
+admin.site.register(Notificacion)
